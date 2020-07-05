b3dfda9106402312849cf70f94f5132906d3f373
Razvan <brb_end1@yahoo.com>
2020-07-05 15:13:10 +0200

    Update push-to-array.js

--- scripts/actions/push-to-array.js
+++ scripts/actions/push-to-array.js
@@ -14,7 +14,7 @@ const pushToArrayHandler = () => {
   }
 
   // push the new value into the shared array
-  _.push(_);
+  inputsArray.push(newInput);
 
 
   console.log('inputsArray (after):', typeof inputsArray, '\n', inputsArray);
